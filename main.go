d1ef5d35548b4e6dff90bffc4daec87d1c76e5e3
WuShaoQiang <592350960@qq.com>
2019-04-09 13:12:19 +0800

    多页爬取

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -10,9 +9,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/chenjiandongx/go-echarts/charts"
-
 	"github.com/PuerkitoBio/goquery"
+	"github.com/chenjiandongx/go-echarts/charts"
 	"github.com/spf13/viper"
 )
 
@@ -25,10 +23,11 @@ type Job struct {
 }
 
 var (
-	headers  = []string{"Host", "User-Agent", "Accept", "Accept-Language", "Accept-Encoding", "Referer", "Connection", "Cookie", "Upgrade-Insecure-Requests", "TE"}
-	keywords = []string{"golang"}
+	headers  = []string{"Host", "User-Agent", "Accept", "Accept-Language", "Accept-Encoding", "Connection"}
+	keywords = []string{"golang", "python"}
 	path     = "/home/shelljo/go/src/github.com/WuShaoQiang/crawler/boss/"
-	// jobs     []Job
+	allJobs  []Job
+	url      = "https://www.zhipin.com%s"
 )
 
 func init() {
@@ -48,115 +47,24 @@ func readHeader() {
 }
 
 func main() {
-	url := "https://www.zhipin.com/job_detail/?query=%s&city=101010100&industry=&position="
+
+	index_page := "/c101010100/?query=%s&page=1&ka=page-1"
 	for index, keyword := range keywords {
+		currentPage := fmt.Sprintf(index_page, keyword)
 		log.Printf("Running on %v\n", index)
-		req, err := http.NewRequest("GET", fmt.Sprintf(url, keyword), nil)
-		if err != nil {
-			log.Fatalf("NewRequest Error: %s", err)
-		}
-
-		for _, header := range headers {
-			req.Header.Add(header, viper.GetString(header))
-		}
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			log.Fatalf("Do Error %s\n", err)
-		}
-
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalf("ReadAll Error %s\n", err)
-		}
-
-		// fmt.Println(string(body))
-		file, err := os.Create(path + keyword + ".html")
-		if err != nil {
-			log.Fatalf("Create File Error %s\n", err)
-		}
-
-		defer file.Close()
-
-		_, err = file.Write(body)
-		if err != nil {
-			log.Fatalf("Write Error %s\n", err)
-		}
-
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
-		if err != nil {
-			log.Fatalf("go query Error %s\n", err)
-		}
-
-		num := doc.Find("ul>li>div.job-primary").Size()
-		fmt.Println(num)
-
-		jobs := make([]Job, num)
-
-		doc.Find("ul>li>div>div>h3>a>span").Each(func(i int, s *goquery.Selection) {
-			str := s.Text()
-			str = strings.Replace(str, "k", "", -1)
-			strs := strings.Split(str, "-")
-			// fmt.Println(strs)
-			num1, _ := strconv.Atoi(strs[0])
-			num2, _ := strconv.Atoi(strs[1])
-
-			jobs[i].Salary = (num1 + num2) / 2
-		})
-
-		doc.Find("ul>li>div>div.info-primary>p").Each(func(i int, s *goquery.Selection) {
-			// fmt.Println(i, s.Text())
-			str := s.Text()
-			// reg := regexp.MustCompile(`[\p{Han}]+`)
-			// fmt.Printf("%q\n", reg.FindAllString(str, -1))
-			// jobs[i].Location = s.Text()
-
-			reg1 := regexp.MustCompile(`\d-\d+`)
-			// fmt.Printf("%q\n", reg1.FindAllString(str, -1))
-			work := reg1.FindAllString(str, -1)
-			if len(work) > 1 {
-				log.Println("workExperience too long")
-			} else if len(work) == 1 {
-				jobs[i].Wrok = work[0]
+		for {
+			next, next_page := getNextPage(currentPage)
+			if !next {
+				break
 			} else {
-				jobs[i].Wrok = "经验不限"
-			}
-
-			// reg2 := regexp.MustCompile(`([\p{Han}]+\s)`)
-			// fmt.Printf("%q\n", reg2.FindAllString(str, -1))
-
-			// nian := fmt.Sprintf("%x", "年")
-			// fmt.Println(nian)
-			reg3 := regexp.MustCompile(`[^\w\s\-]+`)
-			// fmt.Printf("%q\n", reg3.FindAllString(str, -1))
-			temp := reg3.FindAllString(str, -1)
-			location := temp[0]
-			// fmt.Println(location)
-			jobs[i].Location = location
-		})
-
-		fmt.Println(jobs)
-		count := make([]int, 0)
-		nameItems := make([]string, 0)
-		countMap := make(map[string]int)
-		for _, single := range jobs {
-			if _, ok := countMap[strconv.Itoa(single.Salary)]; !ok {
-				countMap[strconv.Itoa(single.Salary)] = 1
-				nameItems = append(nameItems, strconv.Itoa(single.Salary))
-			} else {
-				countMap[strconv.Itoa(single.Salary)]++
+				currentPage = next_page
 			}
 		}
 
-		for _, single := range nameItems {
-			count = append(count, countMap[single])
-		}
-
-		show(nameItems, count)
+		fmt.Println(allJobs)
+		show(counter())
 
 	}
-
 }
 
 func show(nameItems []string, count []int) {
@@ -169,4 +77,106 @@ func show(nameItems []string, count []int) {
 		log.Printf("show Error %s\n", err)
 	}
 	bar.Render(f)
+}
+
+func getNextPage(page string) (bool, string) {
+	var next_page string
+	resp, err := http.Get(fmt.Sprintf(url, page))
+	if err != nil {
+		log.Fatalf("Get Error %s\n", err)
+	}
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatalf("go query Error %s\n", err)
+	}
+
+	loadData(doc)
+	// <a href="javascript:;" ka="page-next" class="next disabled"></a>
+	// <a href="/c101010100/?query=golang&amp;page=10" ka="page-next" class="next"></a>
+	// fmt.Println(doc.Find("a[class=next]").Html())
+	if doc.Find("a[class=next]").Size() == 1 {
+		doc.Find("a[class=next]").Each(func(i int, s *goquery.Selection) {
+
+			// fmt.Println(i, s.Get(i).Attr[0].Val+"&"+s.Get(i).Attr[1].Val)
+			next_page = s.Get(i).Attr[0].Val + "&" + s.Get(i).Attr[1].Val
+		})
+		return true, next_page
+	}
+	return false, ""
+}
+
+func loadData(doc *goquery.Document) error {
+	num := doc.Find("ul>li>div.job-primary").Size()
+	// fmt.Println(num)
+
+	jobs := make([]Job, num)
+
+	doc.Find("ul>li>div>div>h3>a>span").Each(func(i int, s *goquery.Selection) {
+		str := s.Text()
+		str = strings.Replace(str, "k", "", -1)
+		strs := strings.Split(str, "-")
+		// fmt.Println(strs)
+		num1, _ := strconv.Atoi(strs[0])
+		num2, _ := strconv.Atoi(strs[1])
+
+		jobs[i].Salary = (num1 + num2) / 2
+	})
+
+	doc.Find("ul>li>div>div.info-primary>p").Each(func(i int, s *goquery.Selection) {
+		// fmt.Println(i, s.Text())
+		str := s.Text()
+		// reg := regexp.MustCompile(`[\p{Han}]+`)
+		// fmt.Printf("%q\n", reg.FindAllString(str, -1))
+		// jobs[i].Location = s.Text()
+
+		reg1 := regexp.MustCompile(`\d-\d+`)
+		// fmt.Printf("%q\n", reg1.FindAllString(str, -1))
+		work := reg1.FindAllString(str, -1)
+		if len(work) > 1 {
+			log.Println("workExperience too long")
+		} else if len(work) == 1 {
+			jobs[i].Wrok = work[0]
+		} else {
+			jobs[i].Wrok = "经验不限"
+		}
+
+		// reg2 := regexp.MustCompile(`([\p{Han}]+\s)`)
+		// fmt.Printf("%q\n", reg2.FindAllString(str, -1))
+
+		// nian := fmt.Sprintf("%x", "年")
+		// fmt.Println(nian)
+		reg3 := regexp.MustCompile(`[^\w\s\-]+`)
+		// fmt.Printf("%q\n", reg3.FindAllString(str, -1))
+		temp := reg3.FindAllString(str, -1)
+		location := temp[0]
+		// fmt.Println(location)
+		jobs[i].Location = location
+	})
+
+	allJobs = append(allJobs, jobs...)
+
+	return nil
+}
+
+func counter() (nameItems []string, count []int) {
+	// count := make([]int, 0)
+	// nameItems := make([]string, 0)
+	countMap := make(map[string]int)
+	for _, single := range allJobs {
+		if _, ok := countMap[strconv.Itoa(single.Salary)]; !ok {
+			countMap[strconv.Itoa(single.Salary)] = 1
+			nameItems = append(nameItems, strconv.Itoa(single.Salary))
+		} else {
+			countMap[strconv.Itoa(single.Salary)]++
+		}
+	}
+
+	for _, single := range nameItems {
+		count = append(count, countMap[single])
+	}
+
+	return
 }
